2f6c35ddbfae10b6c1b1afa9a4c3567ec8253b97
rousseau-yamil <95605438+rousseau-yamil@users.noreply.github.com>
2025-07-31 08:01:08 -0300

    Update config.js

--- src/configuracion/config.js
+++ src/configuracion/config.js
@@ -13,8 +13,7 @@ export const config = {
   // MONGO_HOST: '127.0.0.1',
   // MONGO_PORT: 27017,
   // DB_NAME: 'entrega-final',
-  MONGO_URL: "mongodb+srv://user_coder:user_coder@cluster0.alfrzlc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  DB_NAME: 'Coderhouse25',
+  
 
 
   // get MONGO_URL() {
@@ -22,5 +21,4 @@ export const config = {
   //   return `mongodb://${this.MONGO_HOST}:${this.MONGO_PORT}/${this.DB_NAME}`;
   // },
 
-  SESSION_SECRET: 'mi_clave_super_secreta'
 };
