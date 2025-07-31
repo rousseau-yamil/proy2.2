3711722fc192f27b4f9c3a20a77e794088c004a8
rousseau-yamil <95605438+rousseau-yamil@users.noreply.github.com>
2025-07-31 08:01:24 -0300

    Update config.js

--- src/configuracion/config.js
+++ src/configuracion/config.js
@@ -2,10 +2,7 @@
 //     PORT:3000,
 //     //MONGO_PORT:27017,
 
-//     //MONGO_URL:"mongodb+srv://coderhouse:codercoder2023@cluster0.wpxpupc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-//     MONGO_URL:'mongodb://127.0.0.1:27017/',
-//     DB_NAME:'entrega-final',
-//     SECRET_SESSION:"mi_clave_super_secreta",
+
 // }
 //mongodb+srv://<db_username>:<db_password>@cluster0.alfrzlc.mongodb.net/
 
